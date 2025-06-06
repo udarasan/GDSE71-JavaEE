6aa527aab4f65fa05748538593c34ecf008994f5
Udara San <udarassanjeewa@gmail.com>
2025-06-06 10:35:54 +0530

    InBuildDBCP completed

---
+++ App1/src/main/java/org/example/InBuildDBCP.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import jakarta.annotation.Resource;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@WebServlet("/inbuild")
+public class InBuildDBCP extends HttpServlet {
+
+    @Resource(name = "java:comp/env/jdbc/pool")
+    private DataSource ds;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            Connection conn = ds.getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
